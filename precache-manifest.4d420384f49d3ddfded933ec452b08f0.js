74032e1cafc8f6dbdff1647044d0bf2857fb40fc
Kay Merchant <58919421+kaymerchant@users.noreply.github.com>
2020-08-02 21:41:26 -0400

    Updates

---
+++ precache-manifest.4d420384f49d3ddfded933ec452b08f0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96d32580a9965a06fadba4c74178cf4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f0a1833626bca799d40d",
+    "url": "/portfolio/static/css/2.14a18772.chunk.css"
+  },
+  {
+    "revision": "f6a25ec85bd5c7a18b6e",
+    "url": "/portfolio/static/css/main.5df75ba0.chunk.css"
+  },
+  {
+    "revision": "f0a1833626bca799d40d",
+    "url": "/portfolio/static/js/2.fcaecbb2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.fcaecbb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a25ec85bd5c7a18b6e",
+    "url": "/portfolio/static/js/main.045cbfde.chunk.js"
+  },
+  {
+    "revision": "64bbae0ef4a0ed30749c",
+    "url": "/portfolio/static/js/runtime-main.ea4809af.js"
+  },
+  {
+    "revision": "7801ea4c2ec9cea601ed776373d1e9c5",
+    "url": "/portfolio/static/media/communicator.7801ea4c.PNG"
+  },
+  {
+    "revision": "fd487e61f85c90b3275d085906f1569d",
+    "url": "/portfolio/static/media/creative.fd487e61.PNG"
+  },
+  {
+    "revision": "97b9afa4d266737ab909c211bce8b856",
+    "url": "/portfolio/static/media/cupCakeLogo.97b9afa4.png"
+  },
+  {
+    "revision": "13e44aba050f3618d57bdf924567e53e",
+    "url": "/portfolio/static/media/dataLogo.13e44aba.png"
+  },
+  {
+    "revision": "185d3909dcf7d2643337ac975b3f7ed0",
+    "url": "/portfolio/static/media/gifLogo.185d3909.png"
+  },
+  {
+    "revision": "6c6850f59ccf70aec68996e35fd5ce7f",
+    "url": "/portfolio/static/media/learner.6c6850f5.PNG"
+  },
+  {
+    "revision": "14a96049d360f26c116cdfb784bec69f",
+    "url": "/portfolio/static/media/logical.14a96049.PNG"
+  },
+  {
+    "revision": "11fee9ea6f6933461d2db25da61432e8",
+    "url": "/portfolio/static/media/problem solver.11fee9ea.PNG"
+  },
+  {
+    "revision": "372cb958a1b7e975dab4a21a62731d53",
+    "url": "/portfolio/static/media/profilePic.372cb958.jpg"
+  },
+  {
+    "revision": "63db4491d0d0ce27aa0ee9e0d4aaedbc",
+    "url": "/portfolio/static/media/readerLogo.63db4491.png"
+  },
+  {
+    "revision": "ae39e71709f8d21750cdce75d1c840bc",
+    "url": "/portfolio/static/media/sandwichLogo.ae39e717.png"
+  },
+  {
+    "revision": "f1eb6d147f4208641cd656561e8d0ee8",
+    "url": "/portfolio/static/media/team player.f1eb6d14.PNG"
+  },
+  {
+    "revision": "d8e3642527e93df420b78941cc1481a1",
+    "url": "/portfolio/static/media/tech.d8e36425.PNG"
+  }
+]);
